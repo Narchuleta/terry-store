8f1626f80f632b92aab9ec82f336c86176f20155
Nicole Archuleta <archuletanola@gmail.com>
2017-11-22 14:35:10 +0000

    Initial commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   belongs_to :brand
   belongs_to :category
   
-  has_attached_file :avatar, styles: ( medium: "300x300#", thumb: "100x100#")
+  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#"}
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
 
