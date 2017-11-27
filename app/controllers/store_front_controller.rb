33ed8915e6e674c1292e0d0c0f1f81b9f07171e1
Nicole Archuleta <archuletanola@gmail.com>
2017-11-27 16:54:40 +0000

    Add search to storefront

--- app/controllers/store_front_controller.rb
+++ app/controllers/store_front_controller.rb
@@ -1,6 +1,12 @@
 class StoreFrontController < ApplicationController
   def index
-    if params[:category_id]
+    if params[:search]
+      @products = Prodduct.search_by_name_or_description(params[:search])
+      if @products.empty?
+        flash.now[:notice] = "No result found for '#{params[:search]}', showing all products."
+        @products = Product.all
+      end
+    elseif params[:category_id]
       @category = Category.find(params[@category.id])
       @products = Product.where(category_id: @category_id)
     else
