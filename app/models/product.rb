e632faf9a2b1faa76ff2546ebf09b77e88a4c173
Nicole Archuleta <archuletanola@gmail.com>
2017-11-28 14:53:13 +0000

    Commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,20 @@
 class Product < ApplicationRecord
   belongs_to :brand
   belongs_to :category
-  
-  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#"}
+
+  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }
+
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  validates_attachment_content_type :avatar, content_type: /\/image\/.*\z/
-  
+
   validates :name, length: { minimum: 3 }
   validates_presence_of :name, :price, :quantity, :brand, :category
   validates_numericality_of :price, greater_than_or_equal_to: 0.01
   validates_numericality_of :quantity, greater_than_or_equal_to: 0
 
   def self.search_by_name_or_description(string)
-  where("name LIKE ? OR description LIKE ?", "#{string}%")
+    where("name LIKE ? OR description LIKE ?", "%#{string}%", "%#{string}%")
   end
 end
-
 # == Schema Information
 #
 # Table name: products
