bf479d94b4e2c7a405cafa7eabf4b19ec636eaed
Nicole Archuleta <archuletanola@gmail.com>
2017-11-29 16:11:43 +0000

    Update carts

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,13 +1,28 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
   
-  def subtotal
-    line_items.to_a.sum do |item|
-      item.total
+  
+  def add_product(product_id)
+    product = Product.find(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+    
+    if current_item
+      current_item.quantity+= 1
+    else
+      current_item = line_items.build(product: product, price: product.price)
+    end
+    current_item
   end
   
+  def subtotal
+    #line_items.to_a.sum do |item|
+     # item.total
+     
+     line_items.select('SUM(quantity * price) AS sum')[0].sum
   end
+  
 end
+
 
 # == Schema Information
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128150854) do
+ActiveRecord::Schema.define(version: 20171129160313) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -34,12 +34,24 @@ ActiveRecord::Schema.define(version: 20171128150854) do
   create_table "line_items", force: :cascade do |t|
     t.integer "product_id"
     t.integer "cart_id"
-    t.integer "quanity", default: 1
+    t.integer "quantity", default: 1
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "pay_type"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
